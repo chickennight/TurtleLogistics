c875d73e234a8441f4490451aaed7089dafd1fa4
이종민 <john0513@naver.com>
2023-07-19 17:01:14 +0900

    [feat] ERD 기반 dto 생성

---
+++ Web/BE/a204/src/main/java/class2/a204/model/Customer.java
@@ -0,0 +1,61 @@
+package class2.a204.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_num")
+    private Long num;
+
+    @Column(name = "customer_id")
+    private String id;
+
+    private String password;
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public Long getNum() {
+        return num;
+    }
+
+    public void setNum(Long num) {
+        this.num = num;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
