da0388dc9387084e60ed4ae640b729c58c9e6c79
이종민 <john0513@naver.com>
2023-07-19 17:01:14 +0900

    [feat] ERD 기반 dto 생성

---
+++ Web/BE/a204/src/main/java/class2/a204/model/OrderNow.java
@@ -0,0 +1,41 @@
+package class2.a204.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ordernow")
+public class OrderNow {
+    @Id
+    private String orderNum;
+
+    private Boolean status;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "order_num", nullable = false)
+    private Orders orders;
+
+    public String getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(String orderNum) {
+        this.orderNum = orderNum;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Orders getOrders() {
+        return orders;
+    }
+
+    public void setOrders(Orders orders) {
+        this.orders = orders;
+    }
+}
