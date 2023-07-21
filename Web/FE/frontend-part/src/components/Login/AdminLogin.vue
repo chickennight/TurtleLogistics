ba8bc7305af7ebd55b6062e882c716dc4b6af428
haan <haanwhy@gmail.com>
2023-07-21 15:59:23 +0900

    [feat] frontend structure

---
+++ Web/FE/frontend-part/src/components/Login/AdminLogin.vue
@@ -0,0 +1,13 @@
+<template>
+    관리자로그인하는곳
+</template>
+
+<script>
+export default {
+    name: "AdminLogin",
+}
+</script>
+
+<style>
+
+</style>
---
+++ Web/FE/frontend-part/src/store/index.js
@@ -0,0 +1,13 @@
+// import Vue from "vue";
+import { createStore } from "vuex";
+//import axios from "axios";
+//import router from "../router";
+
+const store = createStore({
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {}
+})
+
+export default store
