c3d7f86036319dd1a133267419348ad8977a8129
john0513 <john0513@naver.com>
2023-07-22 23:40:26 +0900

    [feat] 이름형식 자동 변환 어노테이션 및 디펜던시 추가

--- Web/BE/a204/src/main/java/class2/a204/repository/MachineRepository.java
+++ Web/BE/a204/src/main/java/class2/a204/repository/MachineRepository.java
@@ -1,12 +1,7 @@
 package class2.a204.repository;
 
 import class2.a204.entity.Machine;
-import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface MachineRepository extends JpaRepository<Machine, Integer> {
 
