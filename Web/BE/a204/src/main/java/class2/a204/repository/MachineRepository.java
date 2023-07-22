12761d3690f02ce3bfbeec766efe0405bc316757
john0513 <john0513@naver.com>
2023-07-22 11:01:11 +0900

    [feat] 구조 변경 및 리팩토링

--- Web/BE/a204/src/main/java/class2/a204/repository/MachineRepository.java
+++ Web/BE/a204/src/main/java/class2/a204/repository/MachineRepository.java
@@ -2,11 +2,11 @@ package class2.a204.repository;
 
 import class2.a204.entity.Machine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MachineRepository extends JpaRepository<Machine, Long> {
-
     Machine findByMachineId(Integer id);
 }
