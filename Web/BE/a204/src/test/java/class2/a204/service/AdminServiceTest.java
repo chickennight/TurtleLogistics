1fbf5d3f2fb13cbb239af2b7b9f674e0998e8fec
dohyun <yup8229@gmail.com>
2023-07-24 17:29:34 +0900

    [feat] : Jwt 적용시킨 AdminAPI TEST 작성

--- Web/BE/a204/src/test/java/class2/a204/service/AdminServiceTest.java
+++ Web/BE/a204/src/test/java/class2/a204/service/AdminServiceTest.java
@@ -1,75 +1,126 @@
-//package class2.a204.service;
-//
-//import class2.a204.entity.Admin;
-//import class2.a204.repository.AdminRepository;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-//@Transactional
-//public class AdminServiceTest {
-//
-//    private final AdminService adminService;
-//    private final AdminRepository adminRepository;
-//
-//    @Autowired
-//    AdminServiceTest(AdminService adminService, AdminRepository adminRepository){
-//        this.adminService = adminService;
-//        this.adminRepository = adminRepository;
-//    }
-//
-//    @Test
-//    public void findByAdminIdTest() {
-//        // given
-//        Admin admin = new Admin();
-//        admin.setAdminId("admin");
-//        admin.setPassword("password");
-//        admin.setPhoneNumber("01011111111");
-//        admin = adminRepository.save(admin);
-//
-//        // when
-//        Admin foundAdmin = adminRepository.findByAdminId(admin.getAdminId()).get();
-//
-//        // then
-//        assertThat(foundAdmin).isNotNull();
-////        assertThat(foundAdmin.getAdminId()).isEqualTo(admin.getAdminId());
-//    }
-//
-//    @Test
-//    public void registerAdminTest() {
-//        // given
-//        Admin admin = new Admin();
-//        admin.setAdminId("admin");
-//        admin.setPassword("password");
-//        admin.setPhoneNumber("01011111111");
-//
-//        // when
-//        Admin savedAdmin = adminService.registerAdmin(admin);
-//
-//        // then
-//        assertThat(savedAdmin).isNotNull();
-////        assertThat(savedAdmin.getPassword()).isNotEqualTo(admin.getPassword());
-//    }
-//
-//    @Test
-//    public void loginTest() {
-//        // given
-//        Admin admin = new Admin();
-//        admin.setAdminId("admin");
-//        admin.setPassword("password");
-//        admin.setPhoneNumber("01011111111");
-//        admin = adminService.registerAdmin(admin);
-//
-//        // when
-//        boolean result = adminService.login(admin.getAdminId(), "password");
-//
-//        // then
-//        assertThat(result).isTrue();
-//    }
-//}
+package class2.a204.service;
+
+import class2.a204.entity.Admin;
+import class2.a204.jwt.JwtTokenProvider;
+import class2.a204.repository.AdminRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class AdminServiceTest {
+
+    private MockMvc mockMvc;
+
+    private final AdminService AS;
+    private final AdminRepository AR;
+
+    private final OrderService OS;
+
+    private final JwtTokenProvider JP;
+
+    @Autowired
+    AdminServiceTest(AdminService as, AdminRepository ar, OrderService os, JwtTokenProvider jp) {
+        this.AS = as;
+        this.AR = ar;
+        this.OS = os;
+        this.JP = jp;
+    }
+
+    @Test
+    public void findByAdminIdTest() {
+        // given
+        Admin admin = new Admin();
+        admin.setAdminId("test");
+        admin.setPassword("1234");
+        admin.setPhoneNumber("01011111111");
+        AR.save(admin);
+
+        // when
+        Optional<Admin> foundAdmin = AR.findByAdminId(admin.getAdminId());
+
+        // then
+        assertThat(foundAdmin).isNotNull();
+        System.out.println(foundAdmin.get().getAdminId());
+    }
+
+    //Admin 회원가입
+    @Test
+    public void registerAdminTest() {
+        // given
+        Admin admin = new Admin();
+        admin.setAdminId("test");
+        admin.setPassword("1234");
+        admin.setPhoneNumber("01011111111");
+
+        // when
+        Admin registedAdmin = AS.registerAdmin(admin);
+
+        // then
+        assertThat(registedAdmin).isNotNull();
+
+    }
+
+    //Admin 로그인
+    @Test
+    public void loginTest() {
+        // given
+        Admin admin = new Admin();
+        admin.setAdminId("test");
+        admin.setPassword("1234");
+        admin.setPhoneNumber("01011111111");
+        AS.registerAdmin(admin);
+
+        // when
+        String token = AS.login(admin.getAdminId(), "1234");
+
+        // then
+        assertThat(token).isNotNull();
+    }
+
+    //토큰 검증
+    @Test
+    public void TokenVaidationTest() {
+        //given
+        Admin admin = new Admin();
+        admin.setAdminId("test");
+        admin.setPassword("1234");
+        admin.setPhoneNumber("01011111111");
+        AS.registerAdmin(admin);
+
+        //when
+        String token = AS.login(admin.getAdminId(), "1234");
+
+        //then
+        boolean isVaildToken = JP.validateToken(token);
+        assertThat(isVaildToken).isTrue();
+
+        //토큰에서 사용자 가져오기
+        String adminId = JP.getUserID(token);
+        System.out.println("토큰으로 사용자 아이디 가져오기");
+        System.out.println(adminId);
+        assertThat(admin.getAdminId()).isEqualTo(adminId);
+
+
+        //토큰에서 권한 정보 가져오기
+        String Role = JP.getRoleFromToken(token);
+        System.out.println(Role);
+        assertThat(Role).isEqualTo("ROLE_ADMIN");
+
+    }
+}
+
+
+
