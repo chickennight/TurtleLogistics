3c70f5b5d299d6e723b7aaad8fc4868d1ea2fdef
이종민 <john0513@naver.com>
2023-07-24 17:30:04 +0900

    [feat] order 분석 구현 과정에서 문제 발생

--- Web/BE/a204/src/main/java/class2/a204/controller/OrderController.java
+++ Web/BE/a204/src/main/java/class2/a204/controller/OrderController.java
@@ -3,15 +3,20 @@ package class2.a204.controller;
 import class2.a204.dto.NewOrderDto;
 import class2.a204.dto.OrderUpdateDto;
 import class2.a204.entity.Log;
+import class2.a204.entity.OrderDetail;
 import class2.a204.entity.OrderNow;
+import class2.a204.entity.Product;
 import class2.a204.service.MachineService;
 import class2.a204.service.OrderService;
+import class2.a204.service.ProductService;
 import class2.a204.util.ErrorHandler;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @Api(tags = "Order")
@@ -24,18 +29,63 @@ public class OrderController {
 
     private final MachineService MS;
 
+    private final ProductService PS;
+
     @Autowired
-    public OrderController(OrderService os, ErrorHandler er, MachineService ms) {
+    public OrderController(OrderService os, ErrorHandler er, MachineService ms, ProductService ps) {
         OS = os;
         ER = er;
         MS = ms;
+        PS = ps;
     }
 
-    // 분석 형태에 따라서 세분화 예정
-    @GetMapping("/analysis")
-    public ResponseEntity<?> dataAnalysis() {
+    @GetMapping("/analysis/day")
+    public ResponseEntity<?> dataAnalysisDay(@RequestParam("start_day") Date startDay, @RequestParam("end_day") Date endDay) {
         try {
-            return new ResponseEntity<>(OS.data(), HttpStatus.OK);
+            return new ResponseEntity<>(OS.dataDay(startDay, endDay), HttpStatus.OK);
+        } catch (Exception e) {
+            return ER.errorMessage(e);
+        }
+    }
+
+    @GetMapping("/analysis/region")
+    public ResponseEntity<?> dataAnalysisRegion(Integer year, Integer month) {
+        try {
+            return new ResponseEntity<>(OS.dataRegion(year, month), HttpStatus.OK);
+        } catch (Exception e) {
+            return ER.errorMessage(e);
+        }
+    }
+
+    @GetMapping("/start")
+    public ResponseEntity<?> packageList() {
+        try {
+            List<OrderNow> list = OS.findPackageOrders();
+
+            if (list.size() == 0)
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+            List<Map<String, ?>> answer = new ArrayList<>();
+            for (OrderNow on : list) {
+                Map<String, Long> input = new HashMap<>();
+                input.put("order_num", on.getOrder().getOrderNum());
+                List<OrderDetail> detailList = OS.findOrderDetailsBy(on.getOrder().getOrderNum());
+                List<Product> productList = PS.findAll();
+                for (Product p : productList)
+                    input.put(String.valueOf(p.getProductNum()), 0L);
+
+                for (OrderDetail od : detailList)
+                    input.put(String.valueOf(od.getProduct().getProductNum()), (long) od.getProduct().getStock());
+
+
+                input.put("address", (long) on.getOrder().getAddress());
+
+                OS.set2OrderNow(on.getOrder().getOrderNum());
+
+                answer.add(input);
+            }
+
+            return new ResponseEntity<>(answer, HttpStatus.OK);
         } catch (Exception e) {
             return ER.errorMessage(e);
         }
@@ -56,32 +106,28 @@ public class OrderController {
     public ResponseEntity<?> orderUpdate(@RequestBody OrderUpdateDto orderUpdateDto) {
         try {
             OrderNow orderNow = OS.findByOrderNum(orderUpdateDto.getOrderNum());
-            if(OS.findByOrderNum(orderUpdateDto.getOrderNum()) ==null) return new ResponseEntity<>( HttpStatus.BAD_REQUEST);
+            if (OS.findByOrderNum(orderUpdateDto.getOrderNum()) == null)
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             if (orderUpdateDto.getType() == 0) {
                 if (orderUpdateDto.getResult() == 1) {
                     orderNow.setStatus(0);
-                    Log l = new Log();
-                    l.setErrorMessage(orderUpdateDto.getOrderNum() + " 포장 문제발 생");
-                    l.setMachine(MS.findMachine(0));
-                    MS.addLog(l);
+                    MS.addLog(errorLog(orderUpdateDto, "포장 문제 발생"));
                     return new ResponseEntity<>("PROBLEM RECORD COMPLETE", HttpStatus.ACCEPTED);
                 }
-                if (orderNow.getStatus() == 1) {
+                if (orderNow.getStatus() == 2) {
                     orderNow.setStatus(3);
                     OS.updateStatus(orderNow);
                     return new ResponseEntity<>(HttpStatus.OK);
                 } else {
                     orderNow.setStatus(3);
                     OS.updateStatus(orderNow);
+                    MS.addLog(errorLog(orderUpdateDto, "포장 순서 문제 발생"));
                     return new ResponseEntity<>("OMISSION CHECK NEED", HttpStatus.ACCEPTED);
                 }
             } else if (orderUpdateDto.getType() == 1) {
                 if (orderUpdateDto.getResult() == 1) {
                     orderNow.setStatus(0);
-                    Log l = new Log();
-                    l.setErrorMessage(orderUpdateDto.getOrderNum() + " 분류 문제 발생");
-                    l.setMachine(MS.findMachine(0));
-                    MS.addLog(l);
+                    MS.addLog(errorLog(orderUpdateDto, "분류 문제 발생"));
                     return new ResponseEntity<>("PROBLEM RECORD COMPLETE", HttpStatus.ACCEPTED);
                 }
                 if (orderNow.getStatus() == 3) {
@@ -91,6 +137,7 @@ public class OrderController {
                 } else {
                     orderNow.setStatus(4);
                     OS.updateStatus(orderNow);
+                    MS.addLog(errorLog(orderUpdateDto, "분류 순서 문제 발생"));
                     return new ResponseEntity<>("OMISSION CHECK NEED", HttpStatus.ACCEPTED);
                 }
             } else
@@ -98,6 +145,13 @@ public class OrderController {
         } catch (Exception e) {
             return ER.errorMessage(e);
         }
+    }
+
+    private Log errorLog(OrderUpdateDto orderUpdateDto, String errorMessage) {
+        Log l = new Log();
+        l.setErrorMessage(orderUpdateDto.getOrderNum() + " " + errorMessage);
+        l.setMachine(MS.findMachine(0));
+        return l;
     }
 
 }
