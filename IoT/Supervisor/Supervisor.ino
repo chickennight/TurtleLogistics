4ddfa86b7bb17c51e8d5c5cb1cc3b7a595ccfc19
leeseunghyuk <sh2463@gmail.com>
2023-07-25 12:36:56 +0900

    [feature] DC Motor Classify Code Finish

--- IoT/Supervisor/Supervisor.ino
+++ IoT/Supervisor/Supervisor.ino
@@ -3,8 +3,8 @@
 // 2. Rest(Post) -> When Order Finish or Divide Finish, Polling
 // 3. MQTT -> Pub : /sup/add(OrderNo,OrderList), /sup/init(On,Off), /sup/div/info(OrderNo,OrderAddress)
 // 4. MQTT -> Sub : /ord/res(OrderNo,Result), /div/res(OrderNo,Result), /web/power(on,off)
-
-#include <secrets.h>
+#include "TLClient.h"
+#include <HTTPClient.h>
 
 #define THINGNAME "Supervisor"
 // PUBLISH TOPIC
@@ -22,64 +22,42 @@ int order_motor=-1,div_motor=-1;
 unsigned long previousMillis = 0;
 const long interval = 60000; // 1min
 
-WiFiClientSecure wifiClient;
-PubSubClient mqttClient(wifiClient);
 HTTPClient http;
+TLClient supervisor(THINGNAME);
 
-void setupWifi(){
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.println(String("Attempting to connect to SSID: ") + String(WIFI_SSID));
+void GETorder();
+void POSTres(const char* jsonData);
+void Subscribe_callback(char *topic, byte *payload, unsigned int length);
 
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(1000);
-    Serial.println("Connecting...");
-  }
-  Serial.println("WiFi Connect Success!");
-}
-
-void connectAWS()
+void setup() 
 {
-  delay(1000);
-  wifiClient.setCACert(AWS_CERT_CA);
-  wifiClient.setCertificate(SUP_CERT);
-  wifiClient.setPrivateKey(SUP_PRIKEY);
+  Serial.begin(115200);
+  supervisor.setCallback(Subscribe_callback);
+  supervisor.connect_AWS();
+  supervisor.subscribe(TOPIC_ORDER_RES);
+  supervisor.subscribe(TOPIC_DIV_RES);
+  supervisor.subscribe(TOPIC_POWER);
  
-  mqttClient.setServer(AWS_IOT_ENDPOINT, 8883);
-  mqttClient.setCallback(messageReceived);
- 
-  Serial.println("Connecting to AWS IOT");
- 
-  while (!mqttClient.connect(THINGNAME))
-  {
-    Serial.print(".");
-    delay(1000);
-  }
- 
-  if (!mqttClient.connected()) {
-    Serial.println("AWS IoT Timeout!");
-    return;
-  }
-  // Subscribe to a topic
-  mqttClient.subscribe(TOPIC_ORDER_RES);
-  mqttClient.subscribe(TOPIC_DIV_RES);
-  mqttClient.subscribe(TOPIC_POWER);
- 
-  Serial.println("AWS IoT Connected!");
 }
 
+void loop() 
+{
+  supervisor.mqttLoop();
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    GETorder();
+  }
+}
 
 void GETorder(){
   if (WiFi.status() == WL_CONNECTED) 
   {
+    Serial.println("Request Get For Get ORDER LISTS");
     http.begin("http://localhost:8080/order/start");
     int httpCode = http.GET();
     Serial.print("HttpCODE:");
-    Serial.print(httpCode);
-    Serial.print(" From:");
-    Serial.print(GET_URL);
-    Serial.println();
+    Serial.println(httpCode);
 
     if (httpCode == 200) 
     {
@@ -100,6 +78,15 @@ void GETorder(){
             JsonArray ordersArray = jsonDoc.as<JsonArray>();
             if(!ordersArray.isNull() && ordersArray.size() > 0)
               {
+                if(order_motor==-1)
+                  {
+                    order_motor=1;
+                    StaticJsonDocument<10> Motor;
+                    Motor["type"]="1";
+                    char buf3[12];
+                    serializeJson(Motor,buf3); 
+                    supervisor.publish(TOPIC_INIT,buf3);                                     
+                  } 
                 for(JsonObject order:ordersArray)
                 {
                   StaticJsonDocument<200> Data;
@@ -111,8 +98,8 @@ void GETorder(){
                   char buf1[200];
                   serializeJson(Data,buf1);
 
-                  mqttClient.publish(TOPIC_ORD_SCH, buf1);
-                  mqttClient.publish(TOPIC_ORD_VERI, buf1);
+                  supervisor.publish(TOPIC_ORD_SCH, buf1);
+                  supervisor.publish(TOPIC_ORD_VERI, buf1);
 
                   StaticJsonDocument<100> Data2;
                   Data2["orderno"] = order["order_num"];
@@ -120,33 +107,23 @@ void GETorder(){
                   
                   char buf2[100];
                   serializeJson(Data2,buf2);
-                  mqttClient.publish(TOPIC_DIV_VERI,buf2);
-
-                  if(order_motor==-1)
-                  {
-                    order_motor=1;
-                    StaticJsonDocument<10> Motor;
-                    Motor["type"]="1";
-                    char buf3[12];
-                    serializeJson(Motor,buf3); 
-                    mqttClient.publish(TOPIC_INIT,buf3);                                     
-                  } 
+                  supervisor.publish(TOPIC_DIV_VERI,buf2);
                 }
               }
         }
       }
     }
     http.end(); 
-   }
-   else
-   {
-      Serial.println("Wifi is not connected!");
-   }
+  }
+  else
+  {
+    Serial.println("Wifi is not connected!");
+  }
 
 }
 
 void POSTres(const char* jsonData){
-  http.begin(POST_URL);
+  http.begin("http://localhost:8080/result");
   http.addHeader("Content-Type","application/json");
 
   int httpResponseCodePost = http.POST(jsonData);
@@ -159,7 +136,7 @@ void POSTres(const char* jsonData){
   http.end();
 }
 
-void messageReceived(char *topic, byte *payload, unsigned int length)
+void Subscribe_callback(char *topic, byte *payload, unsigned int length)
 {
 
   StaticJsonDocument<200> doc;
@@ -169,47 +146,36 @@ void messageReceived(char *topic, byte *payload, unsigned int length)
   
 
   if(strcmp(topic,TOPIC_ORDER_RES)==0){ // orderno, res
-      Serial.println(topic);
+      Serial.println("Get Result From Order Conveyor");
       Serial.println(res);
       POSTres(res);
   }
   else if(strcmp(topic,TOPIC_DIV_RES)==0){ // orderno, res
-      Serial.println(topic);
+      Serial.println("Get Result From Divide Conveyor");
       Serial.println(res);
       POSTres(res);
   }
   else if(strcmp(topic,TOPIC_POWER)==0){ // type : 0(on),1(off)
-      Serial.println(topic);
-      Serial.println(res);
+      Serial.println("Motor Power Control From User!");
       int val = doc["type"].as<int>();
 
       if((val!=order_motor) && (val!=div_motor)){
-        order_motor*=-1;
-        div_motor*=-1;
-        StaticJsonDocument<20> temp;
-        char buffer[20];
-        temp["type"] = val;
-        serializeJson(temp,buffer);
-        mqttClient.publish("/div/motor", buffer);
-        mqttClient.publish("/ord/motor", buffer);
+        if(val==1 or val==-1){
+          order_motor*=-1;
+          div_motor*=-1;
+          StaticJsonDocument<20> temp;
+          char buffer[20];
+          temp["type"] = val;
+          serializeJson(temp,buffer);
+          supervisor.publish("/div/motor", buffer);
+          supervisor.publish("/ord/motor", buffer);
+
+          Serial.print("Now Order Motor Power : ");
+          Serial.println(order_motor);
+          Serial.print("Now Divider Motor Power : ");
+          Serial.println(div_motor);
+        }
+
       }
-  }
-}
-
-void setup() 
-{
-  Serial.begin(115200);
-  setupWifi();
-  connectAWS();
-}
-
-void loop() 
-{
-  mqttClient.loop();
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    GETorder();
-    // 3초마다 실행할 작업
   }
 }
