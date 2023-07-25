b1bc5e8dc61faf71c03657054486496866232388
이종민 <john0513@naver.com>
2023-07-25 12:13:58 +0900

    [feat] sms 구현 완료 및 분석 수정 완료

--- Web/BE/a204/src/main/java/class2/a204/controller/MachineController.java
+++ Web/BE/a204/src/main/java/class2/a204/controller/MachineController.java
@@ -1,12 +1,12 @@
 package class2.a204.controller;
 
 import class2.a204.dto.LogDto;
+import class2.a204.dto.MessageDTO;
 import class2.a204.dto.Payload;
 import class2.a204.entity.Log;
 import class2.a204.entity.Machine;
-import class2.a204.service.MachineService;
-import class2.a204.service.MqttService;
-import class2.a204.service.OrderService;
+import class2.a204.jwt.JwtTokenProvider;
+import class2.a204.service.*;
 import class2.a204.util.ErrorHandler;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,13 +29,19 @@ public class MachineController {
     private final MqttService mqtt;
     private final ErrorHandler Handler;
     private final OrderService OS;
+    private final SmsService SS;
+    private final AdminService AS;
+    private final JwtTokenProvider JP;
 
     @Autowired
-    public MachineController(MachineService ms, MqttService mqtt, ErrorHandler handler, OrderService os) {
-        this.MS = ms;
+    public MachineController(MachineService ms, MqttService mqtt, ErrorHandler handler, OrderService os, SmsService ss, AdminService as, JwtTokenProvider jp) {
+        MS = ms;
         this.mqtt = mqtt;
-        this.Handler = handler;
+        Handler = handler;
         OS = os;
+        SS = ss;
+        AS = as;
+        JP = jp;
     }
 
     @GetMapping
@@ -81,12 +89,15 @@ public class MachineController {
     }
 
     @PostMapping("/log")
-    public ResponseEntity<?> createLog(@RequestBody LogDto logDto) {
+    public ResponseEntity<?> createLog(@RequestBody LogDto logDto, ServletRequest request) {
         try {
             Log input = new Log();
             input.setErrorMessage(logDto.getErrorMessage());
             input.setMachine(MS.findMachine(logDto.getMachineId()));
             MS.addLog(input);
+            String token = JP.resolveToken((HttpServletRequest) request);
+            MessageDTO sms = new MessageDTO(AS.getAdminPhone(token), input.getMachine() + " 기계 이상 발생");
+            SS.sendSms(sms);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
             return Handler.errorMessage(e);
