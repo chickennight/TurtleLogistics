a373d0ec0ddd5827d0e037194bbc156bce0f234a
Dohyun822 <yup8229@gmail.com>
2023-07-30 20:47:28 +0900

    [feat] 변수명 수정

--- Web/BE/a204/src/main/java/class2/a204/controller/MachineController.java
+++ Web/BE/a204/src/main/java/class2/a204/controller/MachineController.java
@@ -1,9 +1,8 @@
 package class2.a204.controller;
 
-import class2.a204.dto.LogAddDto;
-import class2.a204.dto.LogDto;
-import class2.a204.dto.MessageDTO;
-import class2.a204.dto.Payload;
+import class2.a204.dto.LogAddDTO;
+import class2.a204.dto.LogDTO;
+import class2.a204.dto.PayloadDTO;
 import class2.a204.entity.Log;
 import class2.a204.entity.Machine;
 import class2.a204.jwt.JwtTokenProvider;
@@ -15,8 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,103 +24,104 @@ import java.util.Map;
 @Api(tags = "Machine")
 @RequestMapping("/machine")
 public class MachineController {
-    private final MachineService MS;
-    private final MqttService mqtt;
-    private final ErrorHandler Handler;
-    private final OrderService OS;
-    private final SmsService SS;
-    private final AdminService AS;
-    private final JwtTokenProvider JP;
+    private final MachineService machineService;
+    private final MqttService mqttService;
+    private final ErrorHandler errorHandler;
+    private final OrderService orderService;
+    private final SmsService smsService;
+    private final AdminService adminService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public MachineController(MachineService ms, MqttService mqtt, ErrorHandler handler, OrderService os, SmsService ss, AdminService as, JwtTokenProvider jp) {
-        MS = ms;
-        this.mqtt = mqtt;
-        Handler = handler;
-        OS = os;
-        SS = ss;
-        AS = as;
-        JP = jp;
+    public MachineController(MachineService machineService, MqttService mqttService, ErrorHandler errorHandler, OrderService orderService, SmsService smsService, AdminService adminService, JwtTokenProvider jwtTokenProvider) {
+        this.machineService = machineService;
+        this.mqttService = mqttService;
+        this.errorHandler = errorHandler;
+        this.orderService = orderService;
+        this.smsService = smsService;
+        this.adminService = adminService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @GetMapping
     public ResponseEntity<?> getMachineStatus() {
         try {
-            List<Machine> machineList = MS.findMachineAll();
+            List<Machine> machineList = machineService.findMachineAll();
             if (machineList.size() == 0)
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             else {
-                List<Integer> brokenList = MS.brokenMachine(machineList);
+                List<Integer> brokenList = machineService.brokenMachine(machineList);
                 if (brokenList.size() == 0)
                     return new ResponseEntity<>(machineList, HttpStatus.OK);
                 else {
                     Map<String, List<?>> map = new HashMap<>();
                     map.put("상태", machineList);
-                    List<Log> temp = MS.lastBrokenLogs(brokenList);
-                    List<LogDto> errorLogs = new ArrayList<>();
-                    for (Log l : temp) errorLogs.add(new LogDto(l));
+                    List<Log> temp = machineService.lastBrokenLogs(brokenList);
+                    List<LogDTO> errorLogs = new ArrayList<>();
+                    for (Log l : temp) errorLogs.add(new LogDTO(l));
                     map.put("로그", errorLogs);
-                    List<Log> orderError = OS.findOrderError();
-                    List<LogDto> processErrorLogs = new ArrayList<>();
-                    for (Log l : orderError) processErrorLogs.add(new LogDto(l));
+                    List<Log> orderError = orderService.findOrderError();
+                    List<LogDTO> processErrorLogs = new ArrayList<>();
+                    for (Log l : orderError) processErrorLogs.add(new LogDTO(l));
                     map.put("인식 오류 로그", processErrorLogs);
                     return new ResponseEntity<>(map, HttpStatus.OK);
                 }
             }
         } catch (Exception e) {
-            return Handler.errorMessage(e);
+            return errorHandler.errorMessage(e);
         }
     }
 
     @GetMapping("/log")
     public ResponseEntity<?> getLogs() {
         try {
-            List<Log> temp = MS.findLogAll();
-            List<LogDto> logList = new ArrayList<>();
-            for (Log l : temp) logList.add(new LogDto(l));
+            List<Log> temp = machineService.findLogAll();
+            List<LogDTO> logList = new ArrayList<>();
+            for (Log l : temp) logList.add(new LogDTO(l));
             if (logList.isEmpty())
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             else
                 return new ResponseEntity<>(logList, HttpStatus.OK);
         } catch (Exception e) {
-            return Handler.errorMessage(e);
+            return errorHandler.errorMessage(e);
         }
     }
 
     @PostMapping("/log")
-    public ResponseEntity<?> createLog(@RequestBody LogAddDto logAddDto) {
+    public ResponseEntity<?> createLog(@RequestBody LogAddDTO logAddDto) {
         try {
             Log input = new Log();
+            //Log Entity에 메서드 추가
             if (logAddDto.getType() == 0)
-                input.setErrorMessage("포장 파트" + logAddDto.getMachineId() + "기기 이상 발생");
+                input.updateErrorMessage("포장 파트" + logAddDto.getMachineId() + "기기 이상 발생");
             else
-                input.setErrorMessage("분류 파트" + logAddDto.getMachineId() + "기기 이상 발생");
+                input.updateErrorMessage("분류 파트" + logAddDto.getMachineId() + "기기 이상 발생");
 
-            input.setMachine(MS.findMachine(logAddDto.getMachineId()));
-            MS.addLog(input);
+            input.updateMachine(machineService.findMachine(logAddDto.getMachineId()));
+            machineService.addLog(input);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
-            return Handler.errorMessage(e);
+            return errorHandler.errorMessage(e);
         }
     }
 
     @PutMapping
     public ResponseEntity<?> updateMachine(@RequestBody Machine machine) {
         try {
-            MS.updateMachine(machine);
+            machineService.updateMachine(machine);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
-            return Handler.errorMessage(e);
+            return errorHandler.errorMessage(e);
         }
     }
 
     @PostMapping("/control")
-    public ResponseEntity<?> MachineControl(@RequestBody Payload payload) {
+    public ResponseEntity<?> MachineControl(@RequestBody PayloadDTO payload) {
         try {
-            mqtt.publish(payload.getTopic(), payload.getMessage());
+            mqttService.publish(payload.getTopic(), payload.getMessage());
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            return Handler.errorMessage(e);
+            return errorHandler.errorMessage(e);
         }
     }
 
