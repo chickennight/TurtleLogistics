e1bfa4168d4b9a1c08bfa06a0204e0bcb4b6b78e
Dohyun822 <yup8229@gmail.com>
2023-07-31 04:39:59 +0900

    [feat] @JsonProperty 어노테이션 달아주기

--- Web/BE/a204/src/main/java/class2/a204/dto/CustomerLoginDTO.java
+++ Web/BE/a204/src/main/java/class2/a204/dto/CustomerLoginDTO.java
@@ -1,6 +1,7 @@
 package class2.a204.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,7 +12,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class CustomerLoginDTO {
     @ApiModelProperty(value = "아이디", required = true, example = "Customer")
-    private String customer_id;
+    @JsonProperty("customer_id")
+    private String customerId;
     @ApiModelProperty(value = "비밀번호", required = true, example = "Customer")
     private String password;
 }
