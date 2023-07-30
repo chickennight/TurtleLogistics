f2e35b93aec778428efd99138b8f9a8ae785dc2c
Dohyun822 <yup8229@gmail.com>
2023-07-31 04:16:47 +0900

    [feat] Swagger API 문서화

--- Web/BE/a204/src/main/java/class2/a204/controller/MachineController.java
+++ Web/BE/a204/src/main/java/class2/a204/controller/MachineController.java
@@ -2,6 +2,7 @@ package class2.a204.controller;
 
 import class2.a204.dto.LogAddDTO;
 import class2.a204.dto.LogDTO;
+import class2.a204.dto.MachineDTO;
 import class2.a204.dto.PayloadDTO;
 import class2.a204.entity.Log;
 import class2.a204.entity.Machine;
@@ -9,6 +10,7 @@ import class2.a204.jwt.JwtTokenProvider;
 import class2.a204.service.*;
 import class2.a204.util.ErrorHandler;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,6 +45,7 @@ public class MachineController {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
+    @ApiOperation(value = "기기 상태 정보 반환", notes = "모든 기기 상태 정보를 반환하고 에러가 있는 기기가 있다면 에러 기기 로그 기록도 함께 반환")
     @GetMapping
     public ResponseEntity<?> getMachineStatus() {
         try {
@@ -72,6 +75,7 @@ public class MachineController {
         }
     }
 
+    @ApiOperation(value = "모든 로그 가져오기", notes = "저장된 모든 로그를 반환")
     @GetMapping("/log")
     public ResponseEntity<?> getLogs() {
         try {
@@ -87,6 +91,7 @@ public class MachineController {
         }
     }
 
+    @ApiOperation(value = "로그 생성", notes = "새 로그를 생성하고 저장")
     @PostMapping("/log")
     public ResponseEntity<?> createLog(@RequestBody LogAddDTO logAddDto) {
         try {
@@ -105,20 +110,22 @@ public class MachineController {
         }
     }
 
-    @PutMapping
-    public ResponseEntity<?> updateMachine(@RequestBody Machine machine) {
+    @ApiOperation(value = "기기 정보 업데이트", notes = "기기의 정보를 업데이트")
+    @PutMapping("/{machineId}")
+    public ResponseEntity<?> updateMachine(@PathVariable Integer machineId, @RequestBody MachineDTO machineDto) {
         try {
-            machineService.updateMachine(machine);
+            machineService.updateMachine(machineDto, machineId);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
             return errorHandler.errorMessage(e);
         }
     }
 
+    @ApiOperation(value = "기기 제어", notes = "특정 기기를 제어하기 위한 MQTT 메시지를 발행")
     @PostMapping("/control")
-    public ResponseEntity<?> MachineControl(@RequestBody PayloadDTO payload) {
+    public ResponseEntity<?> MachineControl(@RequestBody PayloadDTO payloadDto) {
         try {
-            mqttService.publish(payload.getTopic(), payload.getMessage());
+            mqttService.publish(payloadDto.getTopic(), payloadDto.getMessage());
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             return errorHandler.errorMessage(e);
