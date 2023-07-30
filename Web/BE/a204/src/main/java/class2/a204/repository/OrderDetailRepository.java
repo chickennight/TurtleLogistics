cb8515294453ac56147df00f73d7cf8542aa62a4
Dohyun822 <yup8229@gmail.com>
2023-07-31 04:39:23 +0900

    [feat] JPA쿼리문 주석달기

--- Web/BE/a204/src/main/java/class2/a204/repository/OrderDetailRepository.java
+++ Web/BE/a204/src/main/java/class2/a204/repository/OrderDetailRepository.java
@@ -8,6 +8,13 @@ import java.util.List;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
 
+    /**
+     * 주문번호에 따른 모든 주문 상세 정보 검색
+     *
+     * @param orderNum 주문 상세 정보를 검색할 주문 번호
+     * @return 주어진 주문번호에 해당하는 모든 주문 상세 정보의 리스트
+     *
+     * */
     @Query("SELECT od FROM OrderDetail od WHERE od.order.orderNum = ?1")
     List<OrderDetail> findAllByOrderNum(Long orderNum);
 }
