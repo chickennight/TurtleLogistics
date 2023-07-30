a2552fbf037776a027d1e153d43e09763f169b85
haanwhy <haanwhy@gmail.com>
2023-07-30 21:15:03 +0900

    [feat] 기계 로그 / 상태 수정

--- Web/FE/frontend_part/src/components/BluePrint/MainBluePrint.vue
+++ Web/FE/frontend_part/src/components/BluePrint/MainBluePrint.vue
@@ -8,15 +8,47 @@
         공정현황 버튼
       </v-btn>
   </div>
+  <div class="LogTableContainer">
+    <v-table density="compact" theme="dark">
+      <thead>
+        <tr>
+          <th class="text-left">
+            기계번호
+          </th>
+          <th class="text-left">
+            기계
+          </th>
+          <th class="text-left">
+            고장유무
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(item, index) in machineStatus"
+          :key="index"
+        >
+          <td>{{ item.machine_id }}</td>
+          <td>{{ item.machine_detail }}</td>
+          <td>{{ item.broken }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
 </template>
 
 <script>
+import {mapState} from "vuex";
+
 export default {
     name: "MainBluePrint",
     methods: {
       getMachineStatus(){
         this.$store.dispatch("getMachineStatus");
       }
+    },
+    computed:{
+      ...mapState(["machineStatus"]),
     }
 }
 </script>
