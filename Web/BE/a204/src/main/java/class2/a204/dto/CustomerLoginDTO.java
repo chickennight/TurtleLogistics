89e3a7f4defd942b6d6dac876ff1be3b480e04ee
dohyun <yup8229@gmail.com>
2023-07-31 12:34:30 +0900

    [feat] : @NoArgsConstructor 어노테이션 추가

--- Web/BE/a204/src/main/java/class2/a204/dto/CustomerLoginDTO.java
+++ Web/BE/a204/src/main/java/class2/a204/dto/CustomerLoginDTO.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CustomerLoginDTO {
     @ApiModelProperty(value = "아이디", required = true, example = "Customer")
     @JsonProperty("customer_id")
--- Web/BE/a204/src/main/java/class2/a204/dto/PayloadDTO.java
+++ Web/BE/a204/src/main/java/class2/a204/dto/PayloadDTO.java
@@ -5,6 +5,7 @@ import lombok.*;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PayloadDTO {
     private String topic;
 
