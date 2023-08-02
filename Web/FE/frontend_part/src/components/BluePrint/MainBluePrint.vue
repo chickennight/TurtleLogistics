62ef5d80d2756122a027fc24b234698b8ba675d9
이종민 <john0513@naver.com>
2023-08-02 15:33:53 +0900

    [fix]모듈화오류픽스

--- Web/FE/frontend_part/src/components/BluePrint/MainBluePrint.vue
+++ Web/FE/frontend_part/src/components/BluePrint/MainBluePrint.vue
@@ -1,33 +1,21 @@
 <template>
   <div class="SampleContainer">
     공정현황!!!!(단면도너낌)
-      <v-btn
-      @click="getMachineStatus"
-      background-color='rgb(53, 53, 53)'
-      variant="outlined">
-        공정현황 버튼
-      </v-btn>
+    <v-btn @click="getMachineStatus" background-color="rgb(53, 53, 53)" variant="outlined">
+      공정현황 버튼
+    </v-btn>
   </div>
   <div class="LogTableContainer">
     <v-table density="compact" theme="dark">
       <thead>
         <tr>
-          <th class="text-left">
-            기계번호
-          </th>
-          <th class="text-left">
-            기계
-          </th>
-          <th class="text-left">
-            고장유무
-          </th>
+          <th class="text-left">기계번호</th>
+          <th class="text-left">기계</th>
+          <th class="text-left">고장유무</th>
         </tr>
       </thead>
       <tbody>
-        <tr
-          v-for="(item, index) in machineStatus[`상태`]"
-          :key="index"
-        >
+        <tr v-for="(item, index) in machineStatus[`상태`]" :key="index">
           <td>{{ item.machine_id }}</td>
           <td>{{ item.machine_detail }}</td>
           <td>{{ item.broken }}</td>
@@ -39,25 +27,14 @@
     <v-table density="compact" theme="dark">
       <thead>
         <tr>
-          <th class="text-left">
-            로그번호
-          </th>
-          <th class="text-left">
-            로그 발생 날짜
-          </th>
-          <th class="text-left">
-            메세지
-          </th>
-          <th class="text-left">
-            기계번호
-          </th>
+          <th class="text-left">로그번호</th>
+          <th class="text-left">로그 발생 날짜</th>
+          <th class="text-left">메세지</th>
+          <th class="text-left">기계번호</th>
         </tr>
       </thead>
       <tbody>
-        <tr
-          v-for="item in machineStatus[`로그`]"
-          :key="item.log_num"
-        >
+        <tr v-for="item in machineStatus[`로그`]" :key="item.log_num">
           <td>{{ item.log_num }}</td>
           <td>{{ item.error_date }}</td>
           <td>{{ item.error_message }}</td>
@@ -70,22 +47,13 @@
     <v-table density="compact" theme="dark">
       <thead>
         <tr>
-          <th class="text-left">
-            로그번호
-          </th>
-          <th class="text-left">
-            로그 발생 날짜
-          </th>
-          <th class="text-left">
-            메세지
-          </th>
+          <th class="text-left">로그번호</th>
+          <th class="text-left">로그 발생 날짜</th>
+          <th class="text-left">메세지</th>
         </tr>
       </thead>
       <tbody>
-        <tr
-          v-for="item in machineStatus[`인식 오류 로그`]"
-          :key="item.log_num"
-        >
+        <tr v-for="item in machineStatus[`인식 오류 로그`]" :key="item.log_num">
           <td>{{ item.log_num }}</td>
           <td>{{ item.error_date }}</td>
           <td>{{ item.error_message }}</td>
@@ -96,25 +64,24 @@
 </template>
 
 <script>
-import {mapState} from "vuex";
+import { mapState } from "vuex";
 
 export default {
-    name: "MainBluePrint",
-    data: () => ({
-    }),
-    methods: {
-      getMachineStatus(){
-        this.$store.dispatch("machine/getMachineStatus");
-      }
+  name: "MainBluePrint",
+  data: () => ({}),
+  methods: {
+    getMachineStatus() {
+      this.$store.dispatch("machine/getMachineStatus");
     },
-    computed:{
-      ...mapState(["machineStatus"]),
-    }
-}
+  },
+  computed: {
+    ...mapState("machine", ["machineStatus"]),
+  },
+};
 </script>
 
 <style scoped>
-.SampleContainer{
+.SampleContainer {
   margin: 20px;
   border: 1px solid white;
 }
