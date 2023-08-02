449910d384e4c324bcd9b185f135208e7c03f2b1
yup8229@gmail.com <yup8229@gmail.com>
2023-08-02 12:49:15 +0900

    [fix] 오류&오타 수정

--- Web/FE/frontend_part/src/main.js
+++ Web/FE/frontend_part/src/main.js
@@ -1,16 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 import store from "./store";
 import router from "./router";
 
-loadFonts()
+loadFonts();
 
 const app = createApp(App);
-app.use(router)
-app.use(store)
-app.use(vuetify)
-app.mount('#app');
-
-
+app.use(router);
+app.use(store);
+app.use(vuetify);
+app.mount("#app");
