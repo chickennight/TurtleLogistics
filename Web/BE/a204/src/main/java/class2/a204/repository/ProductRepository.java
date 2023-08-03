6347310469643e9478320773f14645dabb4a6f1b
haan <haanwhy@gmail.com>
2023-08-03 14:56:53 +0900

    [feat] be

--- Web/BE/a204/src/main/java/class2/a204/repository/ProductRepository.java
+++ Web/BE/a204/src/main/java/class2/a204/repository/ProductRepository.java
@@ -2,9 +2,14 @@ package class2.a204.repository;
 
 import class2.a204.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Transactional
+    @Modifying
     @Query("UPDATE Product p SET p.stock = p.stock - ?2 WHERE p.productNum = ?1")
     void updateStock(Integer productNum, Integer stock);
 }
