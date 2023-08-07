555a40b1fae2b64ead622f278151d5846b542145
이종민 <john0513@naver.com>
2023-08-07 21:14:10 +0900

    [BE] image 업로드 다운로드 1차적 구현

---
+++ Web/BE/a204/src/main/java/class2/a204/entity/Image.java
@@ -0,0 +1,35 @@
+package class2.a204.entity;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "image")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private Integer id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "log_num")
+    private Log log;
+
+    private String contentType;
+
+    public Image(Log log, String contentType) {
+        this.log = log;
+        this.contentType = contentType;
+    }
+}
