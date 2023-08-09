0269ff75ceaea6f3f47c981dedc2f0945fcf74b6
haan <haanwhy@gmail.com>
2023-08-09 13:35:56 +0900

    [feat] cctv 기능

---
+++ Web/FE/frontend_part/src/components/Cctv/SampleCCTV.vue
@@ -0,0 +1,43 @@
+<!-- Webcam.vue -->
+<template>
+  <div class="CctvContainer">
+    <video class="VideoContainer" ref="videoElement" autoplay></video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SampleCCTV",
+  data() {
+    return {
+      screenshot: null,
+    };
+  },
+  mounted() {
+    this.initWebcam();
+  },
+  methods: {
+    async initWebcam() {
+      try {
+        const webcamStream = await navigator.mediaDevices.getUserMedia({ video: true });
+        this.$refs.videoElement.srcObject = webcamStream; // 웹캠 비디오 요소
+      } catch (error) {
+        console.error("Error accessing webcam:", error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.CctvContainer {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+}
+
+.VideoContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
