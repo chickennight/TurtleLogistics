548373fb4c4c09529268cbd581c87d4d3966aac4
Dohyun822 <yup8229@gmail.com>
2023-08-13 05:35:55 +0900

    [feat] AdminMainView CSS 수정

--- Web/FE/frontend_part/src/main.js
+++ Web/FE/frontend_part/src/main.js
@@ -5,9 +5,17 @@ import { loadFonts } from "./plugins/webfontloader";
 import store from "./store";
 import router from "./router";
 
+// font-awesome과 관련된 import를 정의
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret, faSort } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserSecret, faSort);
+
 loadFonts();
 
 const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
 app.use(store);
 app.use(vuetify);
