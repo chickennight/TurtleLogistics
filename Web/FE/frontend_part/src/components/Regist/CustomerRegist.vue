7acb8a473ab829ba7338eb3b233d7950c1fea8d0
Dohyun822 <yup8229@gmail.com>
2023-08-18 08:27:51 +0900

    [feat] 회원가입 조건 수정, 및 cctv css 수정

--- Web/FE/frontend_part/src/components/Regist/CustomerRegist.vue
+++ Web/FE/frontend_part/src/components/Regist/CustomerRegist.vue
@@ -13,6 +13,7 @@
             :rules="id_rules"
             label="아이디"
             color="warning"
+            maxlength="15"
             required
           ></v-text-field>
 
@@ -22,6 +23,7 @@
             label="비밀번호"
             type="password"
             color="warning"
+            maxlength="20"
             required
           ></v-text-field>
 
@@ -31,6 +33,7 @@
             label="비밀번호 확인"
             type="password"
             color="warning"
+            maxlength="20"
             required
           ></v-text-field>
 
@@ -38,6 +41,7 @@
             v-model="customer.phone_number"
             :rules="phone_rules"
             label="전화번호"
+            maxlength="11"
             required
           ></v-text-field>
 
@@ -67,11 +71,13 @@ export default {
       (v) => !!v || "해당 칸을 입력해주세요",
       (v) => !(v && v.length <= 7) || "아이디를 8자 이상으로 설정해주세요.",
       (v) => (v && v.trim().length > 0) || "공백만으로는 입력할 수 없습니다.",
+      (v) => !/\s/.test(v) || "공백을 포함할 수 없습니다.",
     ],
     pw_rules: [
       (v) => !!v || "해당 칸을 입력해주세요",
       (v) => !(v && v.length <= 7) || "비밀번호를 8자 이상으로 설정해주세요.",
       (v) => (v && v.trim().length > 0) || "공백만으로는 입력할 수 없습니다.",
+      (v) => !/\s/.test(v) || "공백을 포함할 수 없습니다.",
       (v) =>
         /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/.test(v) ||
         "비밀번호를 영어, 숫자, 특수문자의 조합으로 설정해주세요.",
@@ -81,6 +87,7 @@ export default {
       (v) => !!v || "해당 칸을 입력해주세요",
       (v) => !(v && v.length != 11) || "전화번호가 적합하지 않습니다.",
       (v) => (v && v.trim().length > 0) || "공백만으로는 입력할 수 없습니다.",
+      (v) => !/\D/.test(v) || "숫자만 입력 가능합니다.",
     ],
     select: null,
     region: [
@@ -108,6 +115,15 @@ export default {
     customerRegist() {
       this.$store.dispatch("customer/customerRegist", this.customer);
     },
+    CheckPwd() {
+      if (this.admin.passwordCheck === "") {
+        this.checkMsg = "비밀번호를 확인해주세요.";
+      } else if (this.admin.passwordCheck == this.admin.password) {
+        this.checkMsg = "비밀번호가 일치합니다.";
+      } else {
+        this.checkMsg = "비밀번호가 일치하지 않습니다.";
+      }
+    },
   },
   watch: {
     selectedRegionIndex(newIndex) {
@@ -127,7 +143,7 @@ export default {
 .CustomerContainer {
   display: flex;
   flex-direction: column;
-  height: 200vh;
+  height: 100vh;
   text-align: center;
 }
 .CustomerRegistForm * {
@@ -139,5 +155,14 @@ export default {
   object-fit: contain;
   margin: auto;
   display: block;
+}
+.CustomerRegistForm input[type="number"]::-webkit-inner-spin-button,
+.CustomerRegistForm input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.CustomerRegistForm input[type="number"] {
+  -moz-appearance: textfield;
 }
 </style>
